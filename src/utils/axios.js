1a719e396b9e6dbb8c44e2afa19d84a32e25ed1c
张威 <294341493@qq.com>
2018-07-31 14:57:03 +0800

    ajax拦截

---
+++ src/utils/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+// axios.defaults.headers.common['Authorization'] = 'Bearer' + 'AUTH_TOKEN'
+
+axios.interceptors.request.use(config => {
+  // Do something before request is sent
+  return config
+}, error => {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  // Do something with response data
+  return response
+}, error => {
+  // Do something with response error
+  return Promise.reject(error)
+})
+
+export default axios
