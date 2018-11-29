70433a145f654a0dba9e59d84def657bf8d848a6
张威 <294341493@qq.com>
2018-11-29 11:19:51 +0800

    add pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer'),
+        require('postcss-pxtorem')({
+            rootValue: 20,
+            unitPrecision: 2,
+            propWhiteList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 2
+        })
     ]
 }
